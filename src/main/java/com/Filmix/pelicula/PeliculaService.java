0f20ac259cdb94649dce55d541bf518018cf66e1
Sergio <sergio160502@gmail.com>
2025-07-20 13:08:19 +0200

    first commit

--- src/main/java/com/Filmix/pelicula/PeliculaService.java
+++ src/main/java/com/Filmix/pelicula/PeliculaService.java
@@ -1,8 +1,20 @@
 package com.Filmix.pelicula;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PeliculaService {
+
+	@Autowired
+	PeliculaRepository pr;
+
+	public List<Pelicula> obtenerPeliculas() {
+
+		return pr.findAll();
+
+	}
 
 }
